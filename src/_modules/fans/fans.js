6e81dc553a20e015a1c9ce66ae52c1225930ccaf
alex shopov <alexander.shopov@adelphi.digital>
2018-07-16 15:55:33 +1000

    updates to cookbook fans and header responsive

--- src/_modules/fans/fans.js
+++ src/_modules/fans/fans.js
@@ -5,36 +5,27 @@ export default class Fans {
     this.name = 'fans';
     console.log('%s module', this.name.toLowerCase());
     $('.insta-reel').slick({
-      centerMode: true,
-      adaptiveHeight: true,
       infinite: true,
       dots: false,
       arrows: true,
       slidesToShow: 4,
       slidesToScroll: 1,
-      centerPadding: '100px',
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            centerMode: true,
-            centerPadding: '40px',
           }
         },
         {
           breakpoint: 700,
           settings: {
-            centerMode: true,
-            centerPadding: '40px',
             slidesToShow: 2
           }
         },
         {
           breakpoint: 480,
           settings: {
-            centerMode: true,
-            centerPadding: '40px',
             arrows: false,
             slidesToShow: 1
           }
